0548889491d38741b9bf125f114379ab56b23de4
southocean <hainam2511@gmail.com>
2015-03-07 14:38:11 +0700

    Homework 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   root 'static_pages#home'
+  get 'static_pages/home'
 
   get 'static_pages/help'
 
-  get 'static_pages/about'
-
+  get 'signup' => 'users#new'
+  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
