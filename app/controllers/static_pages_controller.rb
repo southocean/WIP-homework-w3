0a716ce48be72ade09d1ae452e2c27a2251cd575
southocean <hainam2511@Gmail.com>
2015-05-30 17:51:16 +0700

    Homework week 10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,7 @@
 class StaticPagesController < ApplicationController
-
   def home
-    if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
   end
 
   def help
-  end
-
-  def about
-  end
-
-  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,21 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
+  get 'sessions/create'
 
   get 'sessions/new'
 
-  get 'users/new'
-
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
+  root 'static_pages#home' 
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get 'signup' => 'users#new'
 
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy]
-  resources :relationships,       only: [:create, :destroy]
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
